aa66d31bca9e36ad1705ff3857be5c2e178702e8
Daria <daria.shtanko@gmail.com>
2020-06-04 14:57:33 +0200

    Genres done

--- api/genres/controllers.js
+++ api/genres/controllers.js
@@ -1,27 +1,67 @@
-const db = require('../db-connection');
+const db = require("../db-connection");
 
 const controllers = {
   getAll: (req, res) => {
-
     const sql = `SELECT * FROM genres`;
 
     db.all(sql, (err, rows) => {
       if (err) {
-        res.status(400).json({ "error": err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
 
-      res.json(rows)
+      res.json(rows);
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM genres WHERE GenreId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json(rows);
+    });
+  },
   create: (req, res) => {
     // read row data from body
+    const newGenre = req.body;
+    const sql = `INSERT INTO genres (GenreId, Name)
+        VALUES("${newGenre.GenreId}","${newGenre.Name}")`;
+    console.log(newGenre);
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        console.log(err);
+        return;
+      }
+      res.json("New artist is added.");
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const newGenre = req.body;
+    const sql = `UPDATE genres SET Name ='${newGenre.Name}' WHERE GenreId =${newGenre.GenreId}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Value is updated");
+    });
   },
-  delete: (req, res) => { }
-}
+  delete: (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM genres WHERE GenreId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Value is deleted");
+    });
+  },
+};
 
 module.exports = controllers;
