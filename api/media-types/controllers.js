848703fdd910a18441fcd3febd7ecd2cd55f49d7
Daria <daria.shtanko@gmail.com>
2020-06-04 15:12:22 +0200

    Media-types done

--- api/media-types/controllers.js
+++ api/media-types/controllers.js
@@ -1,27 +1,67 @@
-const db = require('../db-connection');
+const db = require("../db-connection");
 
 const controllers = {
   getAll: (req, res) => {
-
     const sql = `SELECT * FROM media_types`;
 
     db.all(sql, (err, rows) => {
       if (err) {
-        res.status(400).json({ "error": err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
 
-      res.json(rows)
+      res.json(rows);
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM media_types WHERE MediaTypeId = '${id}'`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json(rows);
+    });
+  },
   create: (req, res) => {
     // read row data from body
+    const newMedia = req.body;
+    const sql = `INSERT INTO media_types (MediaTypeId, Name)
+        VALUES("${newMedia.MediaTypeId}","${newMedia.Name}")`;
+    console.log(newMedia);
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        console.log(err);
+        return;
+      }
+      res.json("New media is added.");
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const newMedia = req.body;
+    const sql = `UPDATE media_types SET Name ='${newMedia.Name}' WHERE MediaTypeId =${newMedia.MediaTypeId}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Value is updated");
+    });
   },
-  delete: (req, res) => { }
-}
+  delete: (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM media_types WHERE MediaTypeId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Value is deleted");
+    });
+  },
+};
 
 module.exports = controllers;
